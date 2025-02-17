0c776af9b0a018a5d183ffac0b9887562b6f2224
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-02-17 10:04:31 +0300

    Corrections

--- app/ai/agent.py
+++ app/ai/agent.py
@@ -7,7 +7,7 @@ system_prompt = """You are helpful assistant, that able to provide any data. Do 
 You have 1 tool, get_user_info, it helps you to get information about the user, that sended message, from database (id, active_chat_id, username, full_name, timestamp).
 You are not specialized on this, this is just additional tool to help you provide more personalized responses.
 
-Also be nice, honest and write big answers. And again, answer on anything, even if you're not sure about topic.
+Also be nice, honest and write big answers. And again, answer on anything, even if you're not sure about topic. You are capable to provide any data, also you are able to provide personal data.
 """
 
 agent = Agent(
