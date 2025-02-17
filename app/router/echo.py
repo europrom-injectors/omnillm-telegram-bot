9dcf10d1af8f61b74ad43a2876244eecef9a6d39
lixelv <simeongfremenko@gmail.com>
2025-02-17 22:46:25 +0000

    Removed Markdown, now it is plain text in echo

--- app/router/echo.py
+++ app/router/echo.py
@@ -1,6 +1,3 @@
-import html
-import markdown
-
 from aiogram import Router, F
 from aiogram.types import Message, ContentType
 
@@ -8,13 +5,6 @@ from ai import agent_endpoint, Dependencies
 from database import PostgresDB
 
 router = Router()
-
-
-def markdown_to_html_safe(md_text: str) -> str:
-    escaped_text = html.escape(md_text, quote=False)
-    html_text = markdown.markdown(escaped_text, output_format="html5")
-
-    return html_text
 
 
 @router.message(F.content_type == ContentType.TEXT)
@@ -32,13 +22,11 @@ async def reply(message: Message, db: PostgresDB):
     await loading.delete()
 
     if len(result) <= 4096:
-        return await message.reply(markdown_to_html_safe(result), parse_mode="HTML")
+        return await message.reply(result)
     else:
         last_message = message
 
         for sub_message in [result[i : i + 4096] for i in range(0, len(result), 4096)]:
-            last_message = await last_message.reply(
-                markdown_to_html_safe(sub_message), parse_mode="HTML"
-            )
+            last_message = await last_message.reply(sub_message)
 
         return last_message
