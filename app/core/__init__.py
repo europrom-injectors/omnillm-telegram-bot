baa9debcd3ccdb99063f2204eb788842a3fa2937
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-02-25 10:53:51 +0300

    Replaced message texts in core in config.py

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -4,6 +4,8 @@ from .config import (
     TELEGRAM_BOT_TOKEN,
     DATABASE_CONFIG,
     models,
+    start_message,
+    models_info_message,
 )
 from .logging import setup_logging
 from .decorators import (
@@ -20,6 +22,8 @@ __all__ = [
     "async_logfire_decorator",
     "async_logfire_class_decorator",
     "models",
+    "start_message",
+    "models_info_message",
     "APP_NAME",
     "TELEGRAM_BOT_TOKEN",
     "OPENROUTER_TOKEN",
