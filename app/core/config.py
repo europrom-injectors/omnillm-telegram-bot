c2522ab855dd118aa35086446ab704c44af410c0
lixel_ <133605174+lixelv@users.noreply.github.com>
2025-04-18 14:34:04 +0300

    Added mistral gemini 2.5, added recommendations about models. Also added o4 Mini High, removed o1

--- app/core/config.py
+++ app/core/config.py
@@ -18,24 +18,24 @@ DATABASE_CONFIG = {
 }
 
 models = {
-    "OpenAI: o3 Mini High": "openai/o3-mini-high",
-    "OpenAI: o1": "openai/o1-mini",
+    "OpenAI: o4 Mini High": "openai/o4-mini-high",
     "OpenAI: GPT-4o": "openai/gpt-4o-search-preview",
     "OpenAI: GPT-4o-mini": "openai/gpt-4o-mini-search-preview",
     "OpenAI: GPT-4.1": "openai/gpt-4.1",
     "DeepSeek: R1": "deepseek/deepseek-r1",
-    "DeepSeek: DeepSeek V3": "deepseek/deepseek-chat",
-    "Google: Gemini Pro 2.5": "google/gemini-2.5-pro-preview-03-25",
-    "Google: Gemini Flash 2.0": "google/gemini-2.0-flash-001",
-    "Google: Gemini Flash 1.5": "google/gemini-flash-1.5",
+    "DeepSeek: DeepSeek V3": "deepseek/deepseek-chat-v3-0324",
+    "Google: Gemini 2.5 Pro": "google/gemini-2.5-pro-preview-03-25",
+    "Google: Gemini 2.5 Flash": "google/gemini-2.5-flash-preview",
+    "Google: Gemini 2.0 Flash": "google/gemini-2.0-flash-001",
     "Anthropic: Claude 3.7 Sonnet": "anthropic/claude-3.7-sonnet",
     "Anthropic: Claude 3.5 Sonnet": "anthropic/claude-3.5-sonnet",
     "Anthropic: Claude 3.5 Haiku": "anthropic/claude-3.5-haiku",
     "Meta: Llama 4 Maverick": "meta-llama/llama-4-maverick",
     "Meta: Llama 4 Scout": "meta-llama/llama-4-scout",
+    "Mistral: Mistral Large 2411": "mistralai/mistral-large-2411",
     "Amazon: Nova Pro 1.0": "amazon/nova-pro-v1",
     "Amazon: Nova Lite 1.0": "amazon/nova-lite-v1",
-    "xAI: Grok 2 1212": "x-ai/grok-2-1212",
+    "xAI: Grok 3 Mini Beta": "x-ai/grok-3-mini-beta",
 }
 
 agents = {
@@ -65,22 +65,29 @@ start_message = (
 
 models_info_message = (
     "Краткое описание каждой модели:\n\n"
-    "- OpenAI: o3 Mini High: Самая продвинутая модель с технологией потока мысли от OpenAI.\n\n"
-    "- OpenAI: o1: Более простая версия o3, все еще использует технологию потока мысли, но на удивление она дороже, чем o3.\n\n"
-    "- OpenAI: GPT-4o: Обычный ChatGPT, о котором вы так наслышены. Самая популярная модель от OpenAI. Также она поддерживает поиск в интернете из коробки.\n\n"
-    "- OpenAI: GPT-4o-mini: Мини-версия GPT-4o, предназначенная для более легких задач, также она дешевле и быстрее чем GPT-4o.  Также она поддерживает поиск в интернете из коробки.\n\n"
-    "- OpenAI: GPT-4.1: Последняя модель от OpenAI.\n\n"
+    "- OpenAI: o4 Mini High: Самая продвинутая модель с технологией потока мысли от OpenAI.\n\n"
+    "- OpenAI: GPT-4o: Обычный ChatGPT, о котором вы так наслышены. Самая популярная модель от OpenAI. Также она поддерживает поиск в интернете из коробки (Лучшая модель для поиска в интернете).\n\n"
+    "- OpenAI: GPT-4o-mini: Мини-версия GPT-4o, предназначенная для более легких задач, также она дешевле и быстрее чем GPT-4o. Она также умеет искать информацию в интернете, как GPT-4o.\n\n"
+    "- OpenAI: GPT-4.1: Последняя модель GPT от OpenAI, вышла в середине апреля.\n\n"
     "- DeepSeek: R1: Первая модель от DeepSeek, использующая поток мысли, специальная технология позволяющая ИИ думать.\n\n"
     "- DeepSeek: DeepSeek V3: Обычная версия DeepSeek, без технологии потока мысли. Также она дешевле.\n\n"
-    "- Google: Gemini Pro 2.5: Самая продвинута, модель от google, вышла в конце марта, одна из самых последних моделей, бесплатна для пользования.\n\n"
-    "- Google: Gemini Flash 2.0: Усовершенствованная версия языковой модели Gemini с улучшенной скоростью и эффективностью.\n\n"
-    "- Google: Gemini Flash 1.5: Предшественник Gemini Flash 2.0, ориентированный на более быстрые ответы (в 2 раза быстрее, не глупее, чем Gemini Flash 2.0).\n\n"
+    "- Google: Gemini 2.5 Pro: Самая продвинута, модель от google, вышла в середине апреля, одна из самых последних моделей.\n\n"
+    "- Google: Gemini 2.5 Flash : Усовершенствованная версия языковой модели Gemini с улучшенной скоростью и эффективностью.\n\n"
+    "- Google: Gemini 2.0 Flash: Предшественник Gemini Flash 2.5, ориентированный на более быстрые ответы (в 2 раза быстрее, не глупее, чем Gemini Flash 2.5).\n\n"
     "- Anthropic: Claude 3.7 Sonnet: Модель серии Claude, оптимизированная для глубокого понимания и генерации текста. Очень умная модель, лучшая среди Claude.\n\n"
     "- Anthropic: Claude 3.5 Sonnet: Предшественник Anthropic: Claude 3.7 Sonnet.\n\n"
     "- Anthropic: Claude 3.5 Haiku: Как Anthropic: Claude 3.5 Sonnet только быстрее и дешевле, но глупее.\n\n"
-    "- Amazon: Nova Pro 1.0: Профессиональная версия Nova с расширенными возможностями и высокой производительностью.\n\n"
-    "- Amazon: Nova Lite 1.0: Облегченная версия Nova, предназначенная для экономичного использования ресурсов.\n\n"
     "- Meta: Llama 4 Maverick: Самая новая модель от Facebook, с улучшенными возможностями понимания и генерации контента.\n\n"
     "- Meta: Llama 4 Scout: Младшая версия Meta: Llama 4 Maverick.\n\n"
-    "- xAI: Grok 2 1212: Усовершенствованная модель Grok с улучшенными возможностями понимания и генерации контента. Компания xAI принадлежит Илону Маску.\n\n"
+    "- Mistral: Mistral Large 2411: Самая большая модель от Mistral, с улучшенными возможностями понимания и генерации контента. Mistal - европейская компания.\n\n"
+    "- Amazon: Nova Lite 1.0: Облегченная версия Nova, предназначенная для экономичного использования ресурсов.\n\n"
+    "- Amazon: Nova Pro 1.0: Профессиональная версия Nova с расширенными возможностями и высокой производительностью.\n\n"
+    "- xAI: Grok 3 Mini Beta: Усовершенствованная модель Grok с улучшенными возможностями понимания и генерации контента. Компания xAI принадлежит Илону Маску.\n\n\n"
+    
+    "И немного рекомендаций:\n\n"
+    "Для поиска в интернете используйте GPT-4o и GPT-4o-mini.\n"
+    "Для программирования используйте Claude 3.7 Sonnet | GPT-4.1 | Gemini 2.5 Pro.\n"
+    "Для сложных задач, требующих анализа, используйте o4 Mini High.\n"
+    "Для остальных целей отлично подойдет GPT-4.1 и Gemini 2.5 Pro.\n"
+    "Остальные модели можете пробовать по желанию."
 )
